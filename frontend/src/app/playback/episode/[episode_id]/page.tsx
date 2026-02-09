98509d9f4adbe8a2beee87abe5da82e3dab4acfd
Deepak Singh Bungla <nideevl4574@gmail.com>
2026-02-09 12:25:13 +0530

    updated page

---
+++ frontend/src/app/playback/episode/[episode_id]/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import PlayerUI from "@/components/player/PlayerUI";
+import usePlayer from "@/components/player/usePlayer";
+import useSocial from "@/components/player/useSocial";
+
+export default function Page() {
+  const { movie_id } = useParams<{ movie_id: string }>();
+
+  usePlayer(movie_id);
+  useSocial(movie_id);
+
+  return <PlayerUI movie_id={movie_id} />;
+}
